b001a2c60a159ea52d15f4ed8f2b33b6e7786086
Anh Nguyen <anh.t.nguyen07@sjsu.edu>
2022-03-15 00:28:24 -0700

    first commit

--- InorderTraversal.java
+++ InorderTraversal.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class InorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
-        // result list {1,2,3, 4, 5, 6,7}
-        // queue 
+        // result list {4, 2, 5, 1, 6, 3}
+        
         //  1
         //2     3
     // 4    5  6   7
--- PhoneT9.java
+++ PhoneT9.java
@@ -9,6 +9,8 @@ public class PhoneT9 {
     }
     public static List<String> letterCombinations(String digits) {
         LinkedList<String> result = new LinkedList<String>();
+        Queue<String> queue
+            = new LinkedList<>();
         if (digits.length() == 0) return result;
         String[] mapping = {
             "0",
@@ -22,19 +24,21 @@ public class PhoneT9 {
             "tuv",
             "wxyz"
         };
-        // result.add("");
-        // for(int i = 0; i < digits.length(); i++){
-        //     int index = Character.getNumericValue(digits.charAt(i));
-        //     char[] array = mapping[index].toCharArray();
-        //     while(result.peek().length() == i) {
-        //         String previous = result.remove();
-        //         for(char c : array){
-        //             result.add(previous + c);
-        //         }
-        //     }
-        // }
-            letterCombinations(result, digits, "", 0, mapping);
-        return result;
+        queue.add("");
+        for(int i = 0; i < digits.length(); i++){
+            int index = Character.getNumericValue(digits.charAt(i));
+            char[] array = mapping[index].toCharArray();
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                String prev = queue.remove();
+                for (char c : array) {
+                    queue.add(prev + c);
+                }
+            }
+        }
+        ArrayList list = new ArrayList(queue);
+            // letterCombinations(result, digits, "", 0, mapping);
+        return list;
     }
    public static void letterCombinations(List<String> result, String digits, String current, int index, String[] mapping) {
        if(index == digits.length()) {
