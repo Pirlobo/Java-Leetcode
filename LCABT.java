0aed550d40eb2b3b7e59154085584d0f5c02c4bd
Anh Nguyen <anh.t.nguyen07@sjsu.edu>
2022-03-15 03:53:46 -0700

    first commit

--- LCABT.java
+++ LCABT.java
@@ -1,5 +1,12 @@
 public class LCABT {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //                      3 
+        //              5                1
+        //      6           2       0           8
+        //              7      4
+
+        // Test case 1 : 7 8  -> 3
+        // Test case 1 : 5 4  -> 2
         if(root == null || root == p || root == q) return root;
 
         TreeNode left = lowestCommonAncestor(root.left, p, q);
