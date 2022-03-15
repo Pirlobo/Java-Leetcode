683603ad46769434999b614569b92bea271724cc
Anh Nguyen <anh.t.nguyen07@sjsu.edu>
2022-03-14 23:14:45 -0700

    first commit

--- MinimumPathSum.java
+++ MinimumPathSum.java
@@ -4,33 +4,7 @@ public class MinimumPathSum {
         int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
         System.out.println(minPathSum(grid));
     }
-    // public static int minPathSum(int[][] grid) {
-    //     int max = Integer.MAX_VALUE;
-    //     int m = grid.length;
-    //     int n = grid[0].length;
-    //     int[][] dir = {{0,1}, {1, 0}};
-    //     Queue<int[]> q = new LinkedList<int[]>();
-    //      q.add(new int[] {0, 0 , grid[0][0]});
-    //     //  grid[0][0] = -1;
-    //      while (!q.isEmpty()) {
-    //          int size = q.size();
-    //          while(size-- > 0) {
-    //             int[] point = q.poll();
-    //             if(point[0] == m - 1 && point[1] == n - 1){
-    //                 if(point[2] < max) max = point[2];
-    //             }
-    //             for (int[] d : dir) {
-    //                 int r = point[0] + d[0];
-    //                 int c = point[1] + d[1];
-    //                 if(r >= 0 && c >= 0 && r < m && c < n && grid[r][c] >= 0){
-    //                     q.add(new int[]{r,c,point[2] + grid[r][c]} );
-    //                     // grid[r][c] = -1;
-    //                 }
-    //             }
-    //          }
-    //      }
-    //     return max;
-    // } 
+   
     public static int minPathSum(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
@@ -42,10 +16,7 @@ public class MinimumPathSum {
         }
         for (int i = 1; i < rows; i++) {
             for (int j = 1; j < cols; j++) {
-                int smallerNumber = grid[i-1][j];
-                if (grid[i][j-1] < smallerNumber) {
-                    smallerNumber = grid[i][j-1];
-                }
+                int smallerNumber = Math.min(grid[i-1][j], grid[i][j-1]);
                 grid[i][j] = smallerNumber + grid[i][j];
             }
         }
